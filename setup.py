51ca707d84c977af60c9e11b127ba6711895a36c
Steven Lavelle <stevenlavelle@gmail.com>
2019-11-02 12:12:57 -0700

    added pytest.ini

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="package_name",
     version="0.1.0",
-    packages=find_packages()
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    tests_require=[
+        'pytest',
+    ],
 )

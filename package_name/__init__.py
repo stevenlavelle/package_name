8a3e061c4d295ba86c544fee69bea4cdb20e5c8c
Steven Lavelle <stevenlavelle@gmail.com>
2019-11-02 18:55:08 -0700

    setting up tests for app factory

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template
 
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask('package_name')
 
     @app.route('/')
     def home():
         title = 'hello world'
         return render_template('home.html', title=title)
+
+    @app.route('/test')
+    def test():
+        return 'hello world'
 
     return app
--- tests/test_all.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import package_name
-
-def test_app_startup():
-    assert package_name.app is not None

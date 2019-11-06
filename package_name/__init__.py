fb9055855460274ff3056505f621fe5c285cf9f5
Steven Lavelle <stevenlavelle@gmail.com>
2019-11-06 07:46:55 -0800

    update the configuration to run from top-level script

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,24 +1,15 @@
 import os
 
 from flask import Flask, render_template
+from config import config
 
+config_name = os.environ.get('FLASK_ENV')
 
-def create_app(test_config=None):
+def create_app(config_name = 'default'):
     """Create and configure an instance of the Flask application."""
     app = Flask("package_name", instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY="dev",
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "package_name.sqlite"),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     # ensure the instance folder exists
     try:
